3b1b765fb0d8a573f2ab40b706100c223b7c4629
unknown <sattigeri.shubham@outlook.com>
2025-03-02 20:35:09 +0530

    Hi

---
+++ test.py
@@ -0,0 +1,24 @@
+from unit import square
+
+# def main():
+#     test_square()
+
+# def test_square():
+#     if square(2) != 4:
+#         print("2 square is not 4")
+#     if square(3) != 9:
+#         print("3 square is not 9")
+
+# if __name__ == "__main__":
+#     main()        
+# from unit import square
+
+def main():
+    test_square()
+
+def test_square():
+    assert square(2) == 4
+    assert square(3) == 9
+
+if __name__ == "__main__":
+    main()        
